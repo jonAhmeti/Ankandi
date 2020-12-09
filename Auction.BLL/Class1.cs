5de946226f7ab68d6ce262480915527aa0cd23c2
Jon Ahmeti <jon-ahmeti@hotmail.com>
2020-12-09 12:55:12 +0100

    Initial Commit

---
+++ Auction.BLL/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Auction.BLL
+{
+    public class Class1
+    {
+    }
+}
---
+++ Auction.DAL/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Auction.DAL
+{
+    public class Class1
+    {
+    }
+}
