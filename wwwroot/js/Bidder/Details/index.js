af25fadefc07621a92c853506cb066587d431c7d
unknown <jonAhmeti>
2021-01-20 21:06:36 +0100

    Tidied up index.js / javascript file for Bidder/Details/ page. added bidInput validation, added min bid amount info

--- wwwroot/js/Bidder/Details/index.js
+++ wwwroot/js/Bidder/Details/index.js
@@ -1,107 +1,127 @@
 ﻿$(function () {
-
     const urlArray = document.URL.split("?id="); // Get current url
     const eventId = urlArray[urlArray.length - 1];  // Get the last part of the array (-1)
 
     const bidInput = $("#BidInput");
-    let event;
-    function setMinBidInput() {
+    const bidBtn = $("#bid");
+
+    function btnInputValidation(event) {
+        //remove all earlier events so they don't get called twice
+        $(bidInput).off();
+        //bidInput onChange.. disable/enable button
+        $(bidInput).on("change paste keyup blur",
+            function () {
+                if ($(this).val() < event.currentPrice + event.minPriceIncrementAmount) {
+                    $(bidBtn).attr("disabled", true);
+                    $(this).css({ "color": "red"});
+                } else {
+                    $(bidBtn).removeAttr("disabled");
+                    $(this).removeAttr("style");
+                }
+            });
+    }
+
+    function validateInput(event) {
+        if ($(bidInput).val() < event.currentPrice + event.minPriceIncrementAmount) {
+            $(bidBtn).attr("disabled", true);
+            $(bidInput).css({ "color": "red" });
+        } else {
+            $(bidBtn).removeAttr("disabled");
+            $(bidInput).removeAttr("style");
+        }
+        $("#infoIncrement").text(event.currentPrice + event.minPriceIncrementAmount);
+    }
+
+    let refresh;
+    function getEventDetails() {
         $.ajax({
             type: "GET",
             url: "/Bidder/GetEventDetails",
             data: { id: eventId },
             success: function (response) {
-                event = response;
-                $(bidInput).on("change paste keydown keyup",
-                    function (e) {
-                        if ($(this).val() < response.currentPrice + response.minPriceIncrementAmount) {
-                            $(this).val(response.currentPrice + response.minPriceIncrementAmount);
+
+                //set validation onFirstLoad
+                btnInputValidation(response);
+
+                //every 3 seconds get updated info
+                refresh = setInterval(function() {
+                    $.ajax({
+                        type: "GET",
+                        url: "/Bidder/GetEventDetails",
+                        data: {
+                            id: response.id,
+                            username: $("#dropdownProfile").text().trim(),
+                            price: response.currentPrice
+                        },
+                        success: function (response) {
+                            btnInputValidation(response);
+                            $("#CurrentPrice").text(`Current price: ${response.currentPrice}€`);
+                            if (response.priceChanged) {
+                                $("#updatedCurrentPrice").css({ "opacity": 1, "color": "#ffc107" });
+                                $("#updatedCurrentPrice").animate({ opacity: 0 }, 1500);
+                            } else {
+                                $("#updatedCurrentPrice").css({ "opacity": 1, "color": "#17a2b8" });
+                                $("#updatedCurrentPrice").animate({ opacity: 0 }, 1500);
+                            }
+                            validateInput(response);
                         }
                     });
-                $(bidInput).val(response.currentPrice + response.minPriceIncrementAmount);
+                }, 3000);
+
+
+                $("#bid").on("click",
+                    function () {
+                        $.ajax({
+                            type: "POST",
+                            url: "/Bidder/Bid",
+                            data: {
+                                auctionId: response.auctionId,
+                                eventId: response.id,
+                                bidAmount: $("#BidInput").val(),
+                                username: $("#dropdownProfile").text().trim()
+                            },
+                            success: function (response) {
+                                if (response.priceChanged) {
+                                    $("#CurrentPrice").text(`Current price: ${response.currentPrice}€`);
+                                    $("#CurrentPrice").css({ "background-color": "rgba(255,255,0,.7)" });
+                                    $("#CurrentPrice").animate({ backgroundColor: "rgba(255,255,0,0)" }, 1500);
+                                    $("#infoCurrentPrice").css("padding-left", ".5em")
+                                        .html(
+                                            '<i class="fas fa-exclamation-triangle"></i> Someone else placed a bid before the price refreshed!<br />');
+                                } else {
+                                    $("#CurrentPrice").text(`Current price: ${response.currentPrice}€`);
+                                    $("#CurrentPrice").css({ "background-color": "rgba(0,0,255,.7)" });
+                                    $("#CurrentPrice").animate({ backgroundColor: "rgba(0,255,0,0)" }, 1500);
+                                    $("#infoCurrentPrice").removeAttr("style").html("");
+                                }
+                                validateInput(response);
+                            }
+                        });
+                    });
+
+                $("#withdraw").on("click",
+                    function () {
+                        $.ajax({
+                            type: "POST",
+                            url: "/Bidder/Withdraw",
+                            data: {
+                                auctionId: response.auctionId,
+                                eventId: response.id,
+                                username: $("#dropdownProfile").text().trim()
+                            },
+                            success: function (response) {
+                                if (response !== null) {
+                                    $("#CurrentPrice").text(`Current price: ${response.currentPrice}€`);
+                                    $("#CurrentPrice").css({ "background-color": "rgba(255,255,0,.7)" });
+                                    $("#CurrentPrice").animate({ backgroundColor: "rgba(255,255,0,0)" }, 1500);
+                                }
+                                validateInput(response);
+                            }
+                        });
+                    });
             }
         });
     }
 
-    setMinBidInput();
-
-    $("#bid").on("click",
-        function() {
-            $.ajax({
-                type: "POST",
-                url: "/Bidder/Bid",
-                data: {
-                    auctionId: event.auctionId,
-                    eventId: event.id,
-                    bidAmount: $("#BidInput").val(),
-                    username: $("#dropdownProfile").text().trim()
-                },
-                success: function(response) {
-                    if (response.priceChanged) {
-                        $("#CurrentPrice").text(`Current price: ${response.currentPrice}€`);
-                        $("#CurrentPrice").css({ "background-color": "rgba(255,255,0,.7)" });
-                        setMinBidInput();
-                        $("#CurrentPrice").animate({ backgroundColor: "rgba(255,255,0,0)" }, 1500);
-                        $("#infoCurrentPrice").css("padding-left",".5em")
-                            .html(
-                                '<i class="fas fa-exclamation-triangle"></i> Someone else placed a bid before the price refreshed!<br />');
-                    } else {
-                        $("#CurrentPrice").text(`Current price: ${response.currentPrice}€`);
-                        $("#CurrentPrice").css({ "background-color": "rgba(0,0,255,.7)" });
-                        setMinBidInput();
-                        $("#CurrentPrice").animate({ backgroundColor: "rgba(0,255,0,0)" }, 1500);
-                        $("#infoCurrentPrice").removeAttr("style").html("");
-                    }
-                }
-            });
-        });
-
-    $("#withdraw").on("click",
-        function() {
-            $.ajax({
-                type: "POST",
-                url: "/Bidder/Withdraw",
-                data: {
-                    auctionId: event.auctionId,
-                    eventId: event.id,
-                    username: $("#dropdownProfile").text().trim()
-                },
-                success: function (response) {
-                    if (response !== null) {
-                        $("#CurrentPrice").text(`Current price: ${response.currentPrice}€`);
-                        $("#CurrentPrice").css({ "background-color": "rgba(255,255,0,.7)" });
-                        setMinBidInput();
-                        $("#CurrentPrice").animate({ backgroundColor: "rgba(255,255,0,0)" }, 1500);
-                    }
-                }
-            });
-        });
-
-    const refresh = setInterval(function () {
-        $.ajax({
-            type: "GET",
-            url: "/Bidder/GetEventDetails",
-            data: {
-                id: event.id,
-                username: $("#dropdownProfile").text().trim(),
-                price: event.currentPrice
-            },
-            success: function(response) {
-                if (response.priceChanged) {
-                    $("#CurrentPrice").text(`Current price: ${response.currentPrice}€`);
-                    //$("#CurrentPrice").css({ "background-color": "rgba(255,255,0,.7)" });
-                    setMinBidInput();
-                    //$("#CurrentPrice").animate({ backgroundColor: "rgba(255,255,0,0)" }, 1500);
-                    $("#updatedCurrentPrice").css({ "opacity": 1, "color": "#ffc107" });
-                    $("#updatedCurrentPrice").animate({ opacity: 0 }, 1500);
-                } else {
-                    $("#CurrentPrice").text(`Current price: ${response.currentPrice}€`);
-                    setMinBidInput();
-                    $("#updatedCurrentPrice").css({ "opacity": 1, "color": "#17a2b8" });
-                    $("#updatedCurrentPrice").animate({ opacity: 0 }, 1500);
-                }
-            }
-        });
-    }, 3000);
-
+    getEventDetails();
 });
