e5001e1f994620037eb1e05c256f2692803441de
unknown <jonAhmeti>
2021-01-27 15:11:45 +0100

    Final version

--- Areas/Admin/Controllers/HomeController.cs
+++ Areas/Admin/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Auction.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Auction.Areas.Admin.Controllers
@@ -10,9 +11,21 @@ namespace Auction.Areas.Admin.Controllers
     [Route("Admin")]
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly BLL.BidHistories _bllBidHistories;
+        private readonly BLL.Withdraws _bllWithdrawHistories;
+
+        public HomeController(BLL.BidHistories bllBidHistories, BLL.Withdraws bllWithdrawHistories)
         {
-            return View();
+            _bllBidHistories = bllBidHistories;
+            _bllWithdrawHistories = bllWithdrawHistories;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var historyBidWithdraw = new Dictionary<string, object>();
+            historyBidWithdraw["bids"] = Mapper.BidsMap(await _bllBidHistories.GetActiveAuctionBidHistory());
+            historyBidWithdraw["withdraws"] = Mapper.WithdrawsMap(await _bllWithdrawHistories.GetActiveAuctionWithdrawHistory());
+
+            return View(historyBidWithdraw);
         }
     }
 }
